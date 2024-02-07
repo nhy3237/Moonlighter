dcdbcdcd02fd7c54981f0b907e3fb4c96316fb35
nhy3237 <njs3237@naver.com>
2024-02-07 18:26:29 +0900

    Update : Add shop map collider

---
+++ Moonlighter/Assets/Scripts/PlayerLayer.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLayer : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
